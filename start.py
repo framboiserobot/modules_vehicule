4c6d4ae5c0f361214c0e851187e67c8b8a61d134
framboiserobot <56219894+framboiserobot@users.noreply.github.com>
2021-01-24 05:54:23 -0500

    Update start.py

--- start.py
+++ start.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 import os
 
-os.system('/rover/vehiclecontrol/rover_system_module_TCP.py &')
-os.system('/rover/vehiclecontrol/rover_control_module_UDP.py &')
-os.system('/rover/vehiclecontrol/rover_video_module_TCP.py &')
+os.system('/rover/modules_vehicule/rover_system_module_TCP.py &')
+os.system('/rover/modules_vehicule/rover_control_module_UDP.py &')
+os.system('/rover/modules_vehicule/rover_video_module_TCP.py &')
